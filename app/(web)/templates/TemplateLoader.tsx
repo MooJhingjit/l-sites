604627709f58049e4104fbd57604d989d96b7446
pokkrong-webquest <pokkrong@webquest.io>
2024-03-26 10:53:34 +0700

    Refactor TemplateLoader to dynamically import templates based on route pattern

--- app/(web)/templates/TemplateLoader.tsx
+++ app/(web)/templates/TemplateLoader.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { headers } from "next/headers";
-
-const templateWhitelist = ["a-template", "b-template"];
-
 interface Props {
   routePattern: string;
   pageData?: any;
 }
-const getDynamicComponent = (path: string) => dynamic(() => import("" + path));
+const getDynamicComponent = (template: string, routePattern: string) => {
+  // template pattern would be => [main-template]/[sub-template]/[routePattern]
+
+  const fullPath = `./${template}/${routePattern}`
+  return dynamic(async () => {
+  try {
+    // Dynamically import the module using the fullPath
+    const module = await import("" + fullPath);
+    return module.default;
+  } catch (e) {
+    // If the import fails (e.g., the module is not found), fall back to a default template
+    const pathArr = template.split("/");
+    try {
+      // Attempt to import the default template
+      const defaultModule = await import(`./${pathArr[0]}/default/${routePattern}`);
+      return defaultModule.default;
+    } catch (innerError) {
+      // Handle any errors that occur while trying to import the default template
+      console.error('Error loading default module:', innerError);
+      // You could return null or an alternative module/component here as a further fallback
+      return import("./404");
+    }
+  }
+})
+};
 
 export default function TemplateLoader(props: Readonly<Props>) {
   const { routePattern } = props;
@@ -16,12 +37,6 @@ export default function TemplateLoader(props: Readonly<Props>) {
   const url = new URL(urlString ?? "");
   let template = url.searchParams.get("t") as string;
 
-  // check valid template
-  if (!templateWhitelist.includes(template)) {
-    template = "a-template";
-  }
-
-  const DynamicComponent = getDynamicComponent(`./${template}/default/${routePattern}`);
-
+  const DynamicComponent = getDynamicComponent(template, routePattern);
   return <DynamicComponent />;
 }
---
+++ app/(web)/templates/a-template/sorasak/home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SorasakHomePage() {
+  return (
+    <div>this is Sorasak HomePage</div>
+  )
+}
