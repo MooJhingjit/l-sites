50eaba19fb0ccf76655013c24cae271b83fa91c4
pokkrong-webquest <pokkrong@webquest.io>
2024-04-05 10:34:29 +0700

    Refactor getDomain function to use x-url instead of referer on production

--- app/(web)/sites/site.helpers.ts
+++ app/(web)/sites/site.helpers.ts
@@ -2,12 +2,12 @@ import { headers } from "next/headers";
 import { SiteKeys, sites } from "./site.config";
 
 export const getDomain = () => {
-  let referer = headers().get('referer')
-  if (!referer) {
-    referer = 'invalid'
-  }
+  // switch to x-url instead of referer on production
+  let referer = headers().get('referer') ?? "invalid"
+
   const url = new URL(referer);
-  return url.hostname.replace("test.", "") as SiteKeys;
+  const hostname = url.hostname.replace("test.", "");
+  return hostname as SiteKeys
 }
 
 export const getMetaData = (route: string) => {
