3787b6fba845052a243ada04dd123cc122dfd806
pokkrong-webquest <pokkrong@webquest.io>
2024-05-06 16:25:54 +0700

    Refactor component styles, update dependencies, and fix file paths

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,18 +6,42 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function DashboardPage() {
   return (
-    <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-        <Activity className="h-4 w-4 text-muted-foreground" />
-      </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold">+573</div>
-        <p className="text-xs text-muted-foreground">+201 since last hour</p>
-      </CardContent>
-    </Card>
+    <div className="flex space-x-3 ">
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    </div>
+    // <Card>
+    //   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+    //     <CardTitle className="text-sm font-medium">Active Now</CardTitle>
+    //     <Activity className="h-4 w-4 text-muted-foreground" />
+    //   </CardHeader>
+    //   <CardContent>
+    //     <div className="text-2xl font-bold">+573</div>
+    //     <p className="text-xs text-muted-foreground">+201 since last hour</p>
+    //   </CardContent>
+    // </Card>
   )
 }
